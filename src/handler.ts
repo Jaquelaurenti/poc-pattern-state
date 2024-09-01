d33d5376385914a3a0f37344e9c48eb94a5a5c64
Jaque Laurenti <jaquelinelaurenti@gmail.com>
2024-09-01 07:59:17 -0300

    add serverless deploy

---
+++ src/handler.ts
@@ -0,0 +1,8 @@
+import { APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+export const hello = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Hello, world!' }),
+  };
+};
